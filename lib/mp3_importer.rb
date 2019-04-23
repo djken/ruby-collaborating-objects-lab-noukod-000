7c1e4711dec748c9425addb3fb19e6a9a4c7c97c
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-23 13:09:33 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -24,16 +24,16 @@ end
 
 # class MP3Importer
 #   attr_accessor :path
-# 
+#
 #   def initialize(path)
 #     @path = path
 #   end
-# 
+#
 #   def files
 #     Dir[path+"/*.mp3"].map {|music| music.split(path+"/")[1]}
 #     # binding.pry
 #   end
-# 
+#
 #   def import
 #     files.each {|file| Song.new_by_filename(file)}
 #   end
